598975fdb8e5345a829acd74776f53bb26d81555
mahume <michael.a.hume@gmail.com>
2019-12-04 16:53:57 -0700

    Adding folder structure

---
+++ src/utils/styleTemplate.js
@@ -0,0 +1,9 @@
+export const colorPalette = {
+  white: '#e7ecef'
+  grayDark: '#A4A5A0',
+  grayLight: '#CBCBC8',
+  redPale: '#f95854',
+  red: '#bc0008',
+  redDark: '#860007',
+  gold: '#e3ab19',
+}
