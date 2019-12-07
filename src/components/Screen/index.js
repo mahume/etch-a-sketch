b2368aec4f736986f4d4416fdbccf0d41f084723
mahume <michael.a.hume@gmail.com>
2019-12-07 13:09:37 -0700

    Dot appearing randomly on canvas

--- src/components/Screen/index.js
+++ src/components/Screen/index.js
@@ -1,29 +1,27 @@
 import React, { useEffect, useRef} from 'react';
 import { StyledCanvas } from "./styles";
 
+const width = 637;
+const height = 462;
 let x = Math.floor(Math.random() * width);
 let y = Math.floor(Math.random() * height);
 
 const Screen = () => {
-  const canvasRef = useRef(null);
-  const width = 637;
-  const height = 462;
-  const moveAmount = 10;
+  const ref = useRef();
+
+  useEffect(() => {
+    const canvas = ref.current;
+    const ctx = canvas.getContext('2d');
+    ctx.fillRect(x, y, 10, 10);
+  })
+
+  // const moveAmount = 10;
 
   return (
     <StyledCanvas 
-      ref={canvasRef} 
+      ref={ref} 
       width={width} 
       height={height}
-      onClick={e => {
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext('2d');
-        
-        // ctx.beginPath();
-        // ctx.moveTo(x, y);
-        // ctx.lineTo(x, y);
-        // ctx.stroke();
-      }}
     />
   )
 }
