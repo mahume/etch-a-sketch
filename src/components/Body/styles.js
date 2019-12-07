1a68cfcf33b6698c5ba74d97d6d8b01565f0ed59
mahume <michael.a.hume@gmail.com>
2019-12-06 17:42:38 -0700

    Adding shadow to body

--- src/components/Body/styles.js
+++ src/components/Body/styles.js
@@ -8,6 +8,8 @@ export const Main = styled.main`
   border-bottom: 10px solid ${reds.dark};
   border-left: 10px solid ${reds.midLight};
   border-radius: 10px;
+  box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.5),
+              30px 40px 70px 0px rgba(0, 0, 0, 0.3);
 
   grid-row: 2 / 5;
   grid-column: 2 / 5;
