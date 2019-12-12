4512a58c47dd133167c1861cdba264791b8c09b1
mahume <michael.a.hume@gmail.com>
2019-12-12 10:14:53 -0700

    Adding Settings Cog SVG

---
+++ src/components/Cog/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledCog } from './styles';
+
+const Cog = () => {
+  return <StyledCog />
+}
+
+export default Cog;
---
+++ src/components/Cog/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { ReactComponent as Cog } from '../../svg/cog.svg';
+import { reds } from '../../utils/styleTemplate';
+
+export const StyledCog = styled(Cog)`
+  width: 50px;
+  filter: drop-shadow(0px 1px 0.5px ${reds.darkMid});
+  g>g>circle {
+    fill: ${reds.darkMid};
+  }
+
+  grid-row: 3 / 4;
+  grid-column: 2 / 3;
+  justify-self: center;
+  align-self: flex-start;
+`;
