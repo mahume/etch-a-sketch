3ffcb5d7699034e05b2eab1a453af5f32fc6db59
mahume <michael.a.hume@gmail.com>
2019-12-18 16:21:32 -0700

    minor changes to settings card

--- src/components/SettingsCard/styles.js
+++ src/components/SettingsCard/styles.js
@@ -3,13 +3,13 @@ import { grays } from '../../utils/styleTemplate';
 
 export const Canvas = styled.div`
   width: ${({ isViewable }) => isViewable ? '300px' : '0px'};
-  background: linear-gradient(${grays.darkMid}, ${grays.mid});
+  background: linear-gradient(${grays.darkMid}, ${grays.midLight});
   box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.5);
   border-top-left-radius: 30px;
   border-bottom-left-radius: 30px;
 
   transition-property: width;
-  transition-duration: 500ms;
+  transition-duration: 300ms;
   transition-timing-function: ease-out;
 
   grid-row: 3 / 4;
