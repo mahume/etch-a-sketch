c201235acb8f386f3418cf04b00cfe767b69edae
mahume <michael.a.hume@gmail.com>
2019-12-18 13:15:34 -0700

    Settings canvas transition functional

--- src/components/SettingsCard/styles.js
+++ src/components/SettingsCard/styles.js
@@ -2,13 +2,16 @@ import styled, { keyframes } from 'styled-components';
 import { grays } from '../../utils/styleTemplate';
 
 export const Canvas = styled.div`
-  width: 0%; 
+  width: ${({ isViewable }) => isViewable ? '300px' : '0px'};
   background: linear-gradient(${grays.darkMid}, ${grays.mid});
   box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.5);
   border-top-left-radius: 30px;
   border-bottom-left-radius: 30px;
 
-  /* Use transitions or animations */
+  transition-property: width;
+  transition-duration: 500ms;
+  transition-timing-function: ease-out;
+
 
   grid-row: 3 / 4;
   grid-column: 1 / 2;
