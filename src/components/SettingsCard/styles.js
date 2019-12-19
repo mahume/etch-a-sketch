18342020011bca1f71a754850f5eff315b274e16
mahume <michael.a.hume@gmail.com>
2019-12-19 13:48:46 -0700

    Toggle rainbow button functional

--- src/components/SettingsCard/styles.js
+++ src/components/SettingsCard/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { ReactComponent as Rainbow } from '../../svg/rainbow.svg';
+import { ReactComponent as Graybow } from '../../svg/graybow.svg';
 import { grays } from '../../utils/styleTemplate';
 
 export const Canvas = styled.div`
@@ -16,15 +17,40 @@ export const Canvas = styled.div`
   grid-row: 3 / 4;
   grid-column: 1 / 2;
   justify-self: end;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const StyledRainbow = styled(Rainbow)`
   width: 50px;
+  margin-left: 25px;
+  margin-top: 25px;
+  cursor: pointer;
 
   g>g>path.cls-2 {
     fill: #ff931e;
   }
+  g>g>path.cls-3 {
+    fill: #ff0;
+  }
+  g>g>path.cls-4 {
+    fill: #7ac943;
+  }
+  g>g>path.cls-6 {
+    fill: #2e3192;
+  }
+`;
 
-  display: flex;
-  flex-direction: column;
+export const StyledGraybow = styled(Graybow)`
+  width: 50px;
+  margin-left: 25px;
+  margin-top: 25px;
+  cursor: pointer;
+
+  g>g>path.cls-1 {
+    fill: #3e3e40;
+  }
+  g>g>path.cls-2 {
+    fill: #7c7c80;
+  }
 `;
