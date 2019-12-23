c91c327873ee6ab317628908237e226e6feffee3
mahume <michael.a.hume@gmail.com>
2019-12-23 14:23:38 -0500

    Remove testing for shaking removal

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,17 @@
-import React, { useContext } from 'react';
-import Store, { IsErasedContext } from '../../context/Store';
+import React from 'react';
+import Store from '../../context/Store';
 import EtchASketch from '../EtchASketch/index';
 import { GlobalStyles, Grid } from './styles';
 
 function App() {
-  // TypeError: Invalid attempt to destructure non-iterable instance
-  const [isErased] = useContext(IsErasedContext);
-
   return (
     <>
       <GlobalStyles />
-      <Store>
-        <Grid>
+      <Grid>
+        <Store>
           <EtchASketch />
-        </Grid>
-      </Store>
+        </Store>
+      </Grid>
     </>
   );
 }
