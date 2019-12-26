f32783ff83f414aaf7984536e117f94dabe1a71d
mahume <michael.a.hume@gmail.com>
2019-12-26 16:36:35 -0500

    Changing Store Speed onClick functional

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SpeedContext } from '../../context/Store';
 import { StyledSpeedometer } from './styles';
 
 const Speedometer = () => {
+  const [speed, setSpeed] = useContext(SpeedContext);
+
+  const toggleSpeed = () => {
+    const speedOptions = [0.1, 0.5, 1];
+    const currSpeedIndex = speedOptions.findIndex(option => option === speed)
+    if (currSpeedIndex === speedOptions.length - 1) {
+      setSpeed(0.1)
+    } else {
+      const newSpeed = speedOptions[currSpeedIndex + 1]
+      setSpeed(newSpeed)
+    }
+  }
+
   return (
-    <StyledSpeedometer />
+    <StyledSpeedometer 
+      onClick={toggleSpeed}
+    />
   )
 }
 
--- src/components/Speedometer/styles.js
+++ src/components/Speedometer/styles.js
@@ -5,4 +5,9 @@ export const StyledSpeedometer = styled(Speedometer)`
   width: 60px;
   margin-left: 25px;
   cursor: pointer;
+
+  g>g>path.speed-cls-3 {
+    transform-origin: 50% 50%;
+    transform: rotate(-80deg);
+  }
 `;
